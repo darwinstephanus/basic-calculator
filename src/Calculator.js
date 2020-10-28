34a62bea7f691368917c05400a7aaf3878a6dff1
Darwin Stephanus <darwin.stephanus@gmail.com>
2020-10-27 22:54:53 -0400

    adding handleClick for each button and fix operands' logic

--- src/Calculator.js
+++ src/Calculator.js
@@ -9,8 +9,8 @@ export default class Calculator extends React.Component {
     super(props);
     this.state = {
       input: "",
-      previousNumber: "",
-      currentNumber: "",
+      previousNumber: "0",
+      specialEqualNumber: "",
       operator: "",
     };
   }
@@ -36,71 +36,178 @@ export default class Calculator extends React.Component {
   clearInput = () => {
     this.setState({
       input: "",
-      previousNumber: "",
-      currentNumber: "",
+      previousNumber: "0",
+      specialEqualNumber: "0",
       operator: "",
     });
   };
 
   addition = () => {
-    this.setState({ previousNumber: this.state.input });
-    // this.state.previousNumber = this.state.input;
-    this.setState({ input: "" });
-    // this.state.operator = "plus";
     this.setState({ operator: "plus" });
+    if (this.state.input !== "") {
+      this.setState({ previousNumber: this.state.input });
+      this.setState({ input: "" });
+      this.setState({ operator: "plus" });
+      this.evaluate();
+    }
   };
 
   evaluate = () => {
-    // console.log("current Number: ", this.state.currentNumber);
-    // this.setState({ currentNumber: this.state.input });
-    // console.log("current Number: ", this.state.input);
-    // this.state.currentNumber = this.state.input;
     if (this.state.operator === "plus") {
       this.setState({
-        input: parseInt(this.state.previousNumber) + parseInt(this.state.input),
+        previousNumber:
+          parseFloat(this.state.previousNumber) + parseFloat(this.state.input),
       });
     } else if (this.state.operator === "substract") {
       this.setState({
-        input: parseInt(this.state.previousNumber) - parseInt(this.state.input),
+        previousNumber:
+          parseFloat(this.state.previousNumber) - parseFloat(this.state.input),
       });
     } else if (this.state.operator === "multiply") {
       this.setState({
-        input: parseInt(this.state.previousNumber) * parseInt(this.state.input),
+        previousNumber:
+          parseFloat(this.state.previousNumber) * parseFloat(this.state.input),
       });
     } else if (this.state.operator === "divide") {
       this.setState({
-        input: parseInt(this.state.previousNumber) / parseInt(this.state.input),
+        previousNumber:
+          parseFloat(this.state.previousNumber) / parseFloat(this.state.input),
       });
     }
   };
-  //still not working for 1+5+3 or 1-5-3 (more than 2 numbers)
-  substract = () => {
-    this.setState({ previousNumber: this.state.input });
+
+  equalSign = () => {
+    if (this.state.operator === "plus") {
+      this.setState({
+        previousNumber:
+          parseFloat(this.state.previousNumber) +
+          (this.state.input === ""
+            ? parseFloat(this.state.specialEqualNumber)
+            : parseFloat(this.state.input)),
+      });
+      this.setState({
+        specialEqualNumber:
+          this.state.input === ""
+            ? this.state.specialEqualNumber
+            : this.state.input,
+      });
+    } else if (this.state.operator === "substract") {
+      this.setState({
+        previousNumber:
+          parseFloat(this.state.previousNumber) -
+          (this.state.input === ""
+            ? parseFloat(this.state.specialEqualNumber)
+            : parseFloat(this.state.input)),
+      });
+      this.setState({
+        specialEqualNumber:
+          this.state.input === ""
+            ? this.state.specialEqualNumber
+            : this.state.input,
+      });
+    } else if (this.state.operator === "multiply") {
+      this.setState({
+        previousNumber:
+          parseFloat(this.state.previousNumber) *
+          (this.state.input === ""
+            ? parseFloat(this.state.specialEqualNumber)
+            : parseFloat(this.state.input)),
+      });
+      this.setState({
+        specialEqualNumber:
+          this.state.input === ""
+            ? this.state.specialEqualNumber
+            : this.state.input,
+      });
+    } else if (this.state.operator === "divide") {
+      this.setState({
+        previousNumber:
+          parseFloat(this.state.previousNumber) /
+          (this.state.input === ""
+            ? parseFloat(this.state.specialEqualNumber)
+            : parseFloat(this.state.input)),
+      });
+      this.setState({
+        specialEqualNumber:
+          this.state.input === ""
+            ? this.state.specialEqualNumber
+            : this.state.input,
+      });
+    }
     this.setState({ input: "" });
+  };
+
+  substract = () => {
     this.setState({ operator: "substract" });
+    if (this.state.input !== "") {
+      this.setState({ previousNumber: this.state.input });
+      this.setState({ input: "" });
+      this.setState({ operator: "substract" });
+      this.evaluate();
+    }
+    console.log(this.state);
   };
 
   multiply = () => {
-    this.setState({ previousNumber: this.state.input });
-    this.setState({ input: "" });
     this.setState({ operator: "multiply" });
+    if (this.state.input !== "") {
+      this.setState({ previousNumber: this.state.input });
+      this.setState({ input: "" });
+      this.setState({ operator: "multiply" });
+      this.evaluate();
+    }
+    console.log(this.state);
   };
 
   divide = () => {
-    this.setState({ previousNumber: this.state.input });
-    this.setState({ input: "" });
     this.setState({ operator: "divide" });
+    if (this.state.input !== "") {
+      this.setState({ previousNumber: this.state.input });
+      this.setState({ input: "" });
+      this.setState({ operator: "divide" });
+      this.evaluate();
+    }
+    console.log(this.state);
+  };
+
+  plusMinus = () => {
+    //we want to plusMinus the value shown in the screen right now
+    //{this.state.input === ""
+    //? this.state.previousNumber
+    //: this.state.input}
+    if (this.state.input === "") {
+      this.setState({
+        previousNumber:
+          parseFloat(
+            this.state.input === ""
+              ? this.state.previousNumber
+              : this.state.input
+          ) * -1,
+      });
+    } else {
+      this.setState({
+        input: parseFloat(this.state.input) * -1,
+      });
+    }
+  };
+
+  removeLastCharacter = () => {
+    this.setState({ input: this.state.input.slice(0, -1) });
   };
 
   render() {
     return (
       <div className="calc">
         <Row>
-          <Display>{this.state.input}</Display>
+          <Display>
+            {this.state.input === ""
+              ? this.state.previousNumber
+              : this.state.input}
+          </Display>
         </Row>
         <Row>
           <Button handleClick={this.clearInput}>Clear</Button>
-          <Button>←</Button>
+          <Button handleClick={this.removeLastCharacter}>←</Button>
           <Button disabled={true} />
           <Button handleClick={this.addition}>+</Button>
         </Row>
@@ -124,9 +231,9 @@ export default class Calculator extends React.Component {
         </Row>
         <Row>
           <Button handleClick={this.addZeroToInput}>0</Button>
-          <Button>+/-</Button>
+          <Button handleClick={this.plusMinus}>+/-</Button>
           <Button handleClick={this.addDecimal}>.</Button>
-          <Button handleClick={this.evaluate}>=</Button>
+          <Button handleClick={this.equalSign}>=</Button>
         </Row>
       </div>
     );
